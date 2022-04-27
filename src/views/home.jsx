f744989f61defcd85ab746b29a97ca97737d1f0f
Lucas Grass <grassberaldo@gmail.com>
2022-04-27 09:34:22 -0300

    primeiras alteracoes

---
+++ src/views/home.jsx
@@ -0,0 +1,23 @@
+function Home() {
+  return (
+    <div className="flex-container">
+      <div className="container-img">
+        <h1>imagem</h1>
+      </div>
+      <div className="container-button">
+        <button className="button" type="button">Entrada na Gondula</button>
+      </div>
+      <div className="container-button">
+        <button className="button" type="button">Saida da Gondula</button>
+      </div>
+      <div className="container-button">
+        <button className="button">Consultar Gondula</button>
+      </div>
+      <div className="container-exit">
+        <button className="button-exit">Sair</button>
+      </div>
+    </div>
+  )
+}
+
+export default Home
