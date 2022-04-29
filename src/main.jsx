371c49b0949108f657fdc09d6d3a270f068f72ac
Lucas Grass <grassberaldo@gmail.com>
2022-04-29 09:33:03 -0300

    Comeco da tela de entrada na gondola

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './views/home'
+import Gondola_Entry from './views/gondola_entry'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <Gondola_Entry />
   </React.StrictMode>
 )
---
+++ src/views/gondola_entry.jsx
@@ -0,0 +1,18 @@
+function Gondola_Entry() {
+    return (
+      <div className="flex-container">
+          <div className="container-header">
+                <h1 id="title-a">PRODUTOS</h1>
+                <span>
+                    <button id="button-return-menu">Voltar ao Menu</button>
+                </span>
+          </div>
+          <div className="button-move">
+            <button id="button-move-stock">Mover para a Gondula</button>
+          </div>
+      </div>
+    )
+  }
+  
+  export default Gondola_Entry
+  
